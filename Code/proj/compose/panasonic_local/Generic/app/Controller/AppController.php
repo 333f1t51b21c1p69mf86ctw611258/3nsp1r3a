659db9192718f5e529a433fb64a91696ad9997d4
Henrik <henrik@Henriks-MacBook-Pro.local>
2018-02-21 15:34:09 +0700

    Change docker files, htaccess, AppController

--- Code/proj/compose/panasonic_local/Generic/app/Controller/AppController.php
+++ Code/proj/compose/panasonic_local/Generic/app/Controller/AppController.php
@@ -28,7 +28,7 @@ Configure::load('parameters');
 Configure::load('config');
 
 //PHPExcel
-App::import('Vendor','PHPExcel',array('file' => 'excel/phpexcel.php'));
+// App::import('Vendor','PHPExcel',array('file' => 'excel/phpexcel.php'));
 App::import('Vendor','IOFactory',array('file' => 'excel/PHPExcel/IOFactory.php'));
 App::import('Vendor','PHPExcelWriter',array('file' => 'excel/PHPExcel/Reader/Excel5.php'));
 App::import('Vendor','PHPExcelWriter',array('file' => 'excel/PHPExcel/Writer/Excel5.php'));
---
+++ Code/proj/compose/panasonic_local/build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+TAGNAME=briode_core_debug
+
+docker build -t $TAGNAME -t $TAGNAME .
